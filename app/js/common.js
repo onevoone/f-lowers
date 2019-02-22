35fc56cd202e718c94aea2b3f74a04d8654afd6f
onevoone <vovchik0211@gmail.com>
2019-02-22 16:29:32 +0200

    moved obj

--- app/js/common.js
+++ app/js/common.js
@@ -48,7 +48,7 @@ function init() {
 	}
 	
 	var loader = new THREE.OBJLoader();
-	loader.load('../assets/tulip_flower.obj', function (tulip) {
+	loader.load('../tulip_flower.obj', function (tulip) {
 		removePreLoader();
 
 		object = tulip;
